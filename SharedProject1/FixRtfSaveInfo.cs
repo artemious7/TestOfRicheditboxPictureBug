19ad937886a00923e6b210a71ef7b00384b8e5f5
Артем <artem_ty@hotmail.com>
2017-06-09 21:41:36 +0300

    Previous version of the 'fixer' was generating a new bug sometimes, so we're now fixing that bug.

--- SharedProject1/FixRtfSaveInfo.cs
+++ SharedProject1/FixRtfSaveInfo.cs
@@ -3,6 +3,7 @@ using Artemious.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.UI.Text;
@@ -29,7 +30,7 @@ namespace Artemious.RichEditBoxNS
 
         #region Fields
         readonly RichEditBox reb;
-        readonly List<List<char>> picturesFromDocument = new List<List<char>>();
+        readonly List<StringBuilder> picturesFromDocument = new List<StringBuilder>();
         private Func<Task<string>> cleanRtfGenerator;
         #endregion
 
@@ -52,7 +53,7 @@ namespace Artemious.RichEditBoxNS
 
             if (EnableFixing)
             {
-                args.extractPictures = !picturesFromDocument.Any();
+                args.mode = picturesFromDocument.Any() ? FixMode.RestorePictures : FixMode.ExtractPictures;
                 fixRtf(args);
             }
 
@@ -73,28 +74,39 @@ namespace Artemious.RichEditBoxNS
                 {
                     if (EnableFixing && cleanRtfGenerator != null)
                     {
-                        // determine if the document is buggy. Won't change the 'rtfString'.
-                        args.onlyDetermineIfDocumentIsBuggy = true;
-                        fixRtf(args);
+                        if (args.rtfString.StartsWith(@"{\pict"))
+                        {
+                            int rtfDocumentTagIndex = args.rtfString.IndexOf(@"{\rtf");
+                            args.rtfString = args.rtfString.Substring(rtfDocumentTagIndex);
+                            args.mode = FixMode.FixMessup;
+                            fixRtf(args);
+                            args.isBuggy_result = true;
+                        }
+                        else
+                        {
+                            // determine if the document is buggy. Won't change the 'rtfString'.
+                            args.mode = FixMode.DetermineIfDocumentIsBuggy;
+                            fixRtf(args);
+                        }
 
                         if (args.isBuggy_result)
                         {
-                            var args_extractPictures = new ArgumentsForFixRtf { extractPictures = true };
+                            var args_extractPictures = new ArgumentsForFixRtf { mode = FixMode.ExtractPictures };
                             // generate clean RTF
                             args_extractPictures.rtfString = await cleanRtfGenerator();
                             // extract pictures from the clean RTF into 'picturesFromDocument' field.
                             fixRtf(args_extractPictures);
 
-                            args.onlyDetermineIfDocumentIsBuggy = false;
+                            args.mode = FixMode.RestorePictures;
                             // fix the 'rtfString' using the pictures from 'picturesFromDocument'
                             fixRtf(args);
                         }
                     }
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-                    if (System.Diagnostics.Debugger.IsAttached)
-                        System.Diagnostics.Debugger.Break();
+                    //CommonExtensions.DebugBreak();
+                    //EasyTracker.GetTracker().SendException(ex);
                     args.isBuggy_result = false;
                 }
             });
@@ -112,56 +124,81 @@ namespace Artemious.RichEditBoxNS
 
         void fixRtf(ArgumentsForFixRtf args)
         {
-            fixRtf(ref args.rtfString, this.picturesFromDocument, args.extractPictures, args.onlyDetermineIfDocumentIsBuggy, ref args.isBuggy_result);
+            fixRtf(ref args.rtfString, this.picturesFromDocument, ref args.isBuggy_result, args.mode);
         }
 
-        static void fixRtf(ref string rtfString, List<List<char>> pictures, bool extractPictures, bool onlyDetermineIfDocumentIsBuggy, ref bool isBuggy_result)
+        static void fixRtf(ref string rtfString, List<StringBuilder> pictures, ref bool isBuggy_result, FixMode mode)
         {
             #region define variables
 
-            //const int MAX_SYMBOLS_AFTER_LAST_CLOSING_BRACE = 6;
+            #region mode
+            bool extractPictures = false,
+                 findPictures = false,
+                 onlyDetermineIfDocumentIsBuggy = false,
+                 restorePictures = false,
+                 fixMessup = false;
+
+            switch (mode)
+            {
+                case FixMode.ExtractPictures:
+                    extractPictures = findPictures = true;
+                    break;
+                case FixMode.RestorePictures:
+                    restorePictures = true;
+                    break;
+                case FixMode.DetermineIfDocumentIsBuggy:
+                    onlyDetermineIfDocumentIsBuggy = true;
+                    break;
+                case FixMode.FixMessup:
+                    fixMessup = findPictures = true;
+                    break;
+                default:
+                    throw new NotImplementedException(mode.ToString());
+            }
+            #endregion
+
             bool metLastClosingBrace = false;
             const string PICTURE_RTF_TAG = @"{\pict"; // a picture start RTF tag
             int pictRtfIndex = 0; // index of a char in PICTURE_RTF_TAG which is currently being viewed. This is for searching the PICTURE_RTF_TAG in rtfString.
-            List<char> currentPicture = null; // a picture that is currently being extracted. List of characters.
+            StringBuilder currentPicture = null; // a picture that is currently being extracted.
             bool isPicture = false; // is it a picture that is currently being viewed.
             int openBracesCountOfPictureRtfTag = -1; // value of 'opening' when the PICTURE_RTF_TAG is met in rtfString. This is to determine the closing brace '}' in the picture.
-            int pictureToRestore_index = 0; // index of the picture that is being restored.
 
-            bool restorePictures = !extractPictures; // is it restore pictures mode?
-            int[] indexesOfPicturesToRestore = new int[pictures.Count + 1]; // if we are in 'restore pictures' mode, put their indexes within result array.
+            var indexesOfPicturesToRestore = new List<int>(pictures.Count); // if we are in 'restore pictures' mode, put their indexes within result array.
 
             int opening = 0; // count of currently open braces.
             int lastClosingBraceResultIndex = rtfString.Length; // last closing brace index within the result array. Define in with the highest value - last index of rtfString plus one - in order to know if (for some buggy reason) no braces were met during viewing the rtfString.
-            char[] result = new char[rtfString.Length]; // resulting string will be put here. Its length may be a couple characters smaller than rtfString.Length
-            int resultIndex = 0; // current index within the 'result' array 
-
-            if (onlyDetermineIfDocumentIsBuggy)
-                extractPictures = restorePictures = false;
+            var result = new StringBuilder(rtfString); // resulting string will be put here. Its length may be a couple characters smaller than rtfString.Length
             #endregion
 
             #region view all the rtfString characters
 
-            for (int i = 0; i < rtfString.Length; i++)
+            for (int i = 0; i < result.Length; i++)
             {
-                char symbol = rtfString[i];
-
-                // if there are any braces after the last one, then it's a buggy document
-                if (onlyDetermineIfDocumentIsBuggy && metLastClosingBrace && (symbol == '}' || symbol == '{'))
-                {
-                    isBuggy_result = true;
-                    return;
-                }
-
+                char symbol = result[i];
                 switch (symbol)
                 {
                     case '{':
                         // increment the current opening braces count.
                         opening++;
+
+                        // if there are any braces after the last one, then it's a buggy document
+                        if (onlyDetermineIfDocumentIsBuggy && metLastClosingBrace)
+                        {
+                            isBuggy_result = true;
+                            return;
+                        }
                         break;
                     case '}':
                         // decrement the current opening braces count.
                         opening--;
+
+                        // if there are any braces after the last one, then it's a buggy document
+                        if (onlyDetermineIfDocumentIsBuggy && metLastClosingBrace)
+                        {
+                            isBuggy_result = true;
+                            return;
+                        }
 
                         #region removing the odd closing brace
                         // if the last brace gets closed, don't put it to the 'result' array, because there are may be some other characters after this one.
@@ -171,11 +208,13 @@ namespace Artemious.RichEditBoxNS
                         {
                             opening++; // pretend that it is still open
                             // set the position, into which we will later restore the last closing brace in the document.
-                            lastClosingBraceResultIndex = resultIndex;
+                            lastClosingBraceResultIndex = i;
                             if (onlyDetermineIfDocumentIsBuggy)
                                 metLastClosingBrace = true;
-                            if (restorePictures)
-                                indexesOfPicturesToRestore[pictureToRestore_index++] = resultIndex;
+                            else if (restorePictures)
+                                indexesOfPicturesToRestore.Add(i);
+
+                            result.Remove(i--, 1);
                             continue;
                         }
                         #endregion
@@ -187,12 +226,16 @@ namespace Artemious.RichEditBoxNS
                         {
                             // finish this picture extraction
                             isPicture = false;
-                            // add the closing brace '}'
-                            currentPicture.Add(symbol);
+                            // in mode 'fixMessup' we'll leave the odd '}' brace as a flag of picture position - we'll not remove it from the 'result' string
+                            if (!fixMessup)
+                                // add the closing brace '}'
+                                currentPicture.Append(symbol);
                             // free up some memory
-                            currentPicture.Capacity = currentPicture.Count;
+                            currentPicture.Capacity = currentPicture.Length;
                             // remember the picture
                             pictures.Add(currentPicture);
+                            if (fixMessup)
+                                indexesOfPicturesToRestore.Add(i - currentPicture.Length);
                             pictRtfIndex = 0;
                         }
                         #endregion
@@ -200,10 +243,10 @@ namespace Artemious.RichEditBoxNS
                         break;
                 }
 
-                if (extractPictures)
+                if (findPictures)
                 {
                     if (isPicture)
-                        currentPicture.Add(symbol);
+                        currentPicture.Append(symbol);
 
                     #region searching the picture rtf starting tag
                     else
@@ -216,8 +259,8 @@ namespace Artemious.RichEditBoxNS
                             if (pictRtfIndex == PICTURE_RTF_TAG.Length)
                             {
                                 // create the array for picture with the picture starting tag in it.
-                                currentPicture = PICTURE_RTF_TAG.ToList();
-                                currentPicture.Capacity = rtfString.Length;  // reserve the memory as if the picture was taking all the space in rtfString, because we do not know the exact size of the picture. We'll free up the memory later. (This is good only when a document contains 0, 1 or 2 pictures, but it is too much in other cases.)
+                                // reserve the memory as if the picture was taking all the space in rtfString, because we do not know the exact size of the picture. We'll free up the memory later. (This is good only when a document contains 0, 1 or 2 pictures, but it is too much in other cases.)
+                                currentPicture = new StringBuilder(PICTURE_RTF_TAG, rtfString.Length);
                                 isPicture = true;
                                 openBracesCountOfPictureRtfTag = opening;
                             }
@@ -229,62 +272,68 @@ namespace Artemious.RichEditBoxNS
                     }
                     #endregion
                 }
-
-                // put the symbol to the result and increment index in the resulting array
-                result[resultIndex++] = symbol;
             }
             #endregion
 
+            // from now on be careful with index values in result array, because we insert things into the result array, and it is not actual.
 
             #region restore the last closing brace
             // if there was a closing brace that we removed last
             if (lastClosingBraceResultIndex > 0)
-            {
-                // Shift the last characters in result array by 1 position forward - so that we could insert the last closing brace.
-                for (int i = resultIndex - 1; i > lastClosingBraceResultIndex; i--)
-                    result[i] = result[i - 1];
                 // insert the last closing brace
-                result[lastClosingBraceResultIndex] = '}';
-            }
+                result.Insert(lastClosingBraceResultIndex, '}');
             #endregion
 
 
-            if (restorePictures)
+            #region restoring or removing the pictures
+            if (restorePictures || fixMessup)
             {
-                #region restoring the pictures
-                var resultWithPictures = result.ToList();
-                // free up memory
-                result = null;
+                if (restorePictures)
+                    indexesOfPicturesToRestore.Remove(lastClosingBraceResultIndex);
 
-                // insert pictures into the 'resultWithPictures' list - in reverse order, because we are relying on the index (from 'indexesOfPicturesToRestore') of the collection that we change - 'resultWithPictures'.
-                for (int i = pictures.Count - 1; i >= 0; i--)
+                // insert pictures into (or remove from) 'result' - in reverse order by Index, because we are relying on the index (from 'indexesOfPicturesToRestore') of the collection that we change - 'result'.
+                foreach (var p in pictures.Zip(indexesOfPicturesToRestore, (picture, index) => new PictureWithIndex(picture, index)).OrderByDescending(r => r.Index))
                 {
-                    var picture = pictures[i];
-                    int index = indexesOfPicturesToRestore[i];
-                    resultWithPictures.InsertRange(index, picture);
+                    if (fixMessup)
+                        result.Remove(p.Index, p.Picture.Length);
+                    else
+                        result.Insert(p.Index, p.Picture.ToString());
                 }
-
-                rtfString = new string(resultWithPictures.ToArray());
-                #endregion
             }
-            else
-                // return the result as string
-                rtfString = new string(result);
+            #endregion
+
+            if (fixMessup)
+                pictures.Clear();
+
+            // return the result as string
+            rtfString = result.ToString();
+        }
+
+        struct PictureWithIndex
+        {
+            public PictureWithIndex(StringBuilder picture, int index)
+            {
+                Picture = picture;
+                Index = index;
+            }
+
+            public readonly StringBuilder Picture;
+            public readonly int Index;
         }
 
         enum FixMode
         {
             ExtractPictures,
             RestorePictures,
-            OnlyDetermineIfDocumentIsBuggy
+            DetermineIfDocumentIsBuggy,
+            FixMessup
         }
 
         class ArgumentsForFixRtf
         {
             public string rtfString;
             public bool isBuggy_result;
-            public bool extractPictures;
-            public bool onlyDetermineIfDocumentIsBuggy;
+            public FixMode mode;
         }
     }
 }
